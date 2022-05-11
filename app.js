282fbd2b721d40af3de62e7cd08110f8b87f49bb
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-05-11 16:56:35 +0200

    Working

--- app.js
+++ app.js
@@ -3,18 +3,67 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const fs = require("fs");
+const cors = require("cors");
 
 var app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {index: false} ));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// Hämta users
+app.get("/users", (req, res) => {
+
+    fs.readFile("users.json", (error, data) => {
+        
+        if (error) {
+            res.json({ "message": "error" })
+        } else {
+            let users = JSON.parse(data);
+            res.json({ "message": "success", "contacts": users });
+        }
+    }); 
+});
+
+
+// Hämta users
+app.post("/users", (req, res) => {
+
+    // Hämta
+    fs.readFile("users.json", (error, data) => {
+        
+        if (error) {
+            res.json({ "message": "error" })
+        } 
+
+        let users = JSON.parse(data);
+
+        // Ändra
+        let newContact = {
+            "name": req.body.name,
+            "email": req.body.email,
+            "phone": req.body.phone
+        }
+
+        users.push(newContact);
+
+        // Spara
+        fs.writeFile("users.json", JSON.stringify(users), (error) => {
+
+            if (error) {
+                console.log("Något blev fel: " + error)
+            }
+        })
+        res.send(users)
+
+    }); 
+
+});
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+// const fs = require("fs");
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+// router.get("/users", (req, res) => {
+
+//   fs.readFile("./users.json", (error, data) => {
+      
+//       if (error) {
+//           console.log("Error: " + error)
+//       } else {
+//           let users = JSON.parse(data);
+//           res.json(users);
+//       }
+//   });
+
+// });
+
 
 module.exports = router;
